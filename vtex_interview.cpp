3c540993e2883fac68632febe77285bb46ddecd0
leftabn <allanbispoxd@gmail.com>
2021-02-17 19:10:49 -0300

    Fix the first algorithm and add new one from the Coding Interview course

--- vtex_interview.cpp
+++ vtex_interview.cpp
@@ -1,74 +1,85 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
+
+// FIND NEXT GREATER NUMBER WITH SAME SET OF DIGITS
 
 #define MAX 10001
 
 using namespace std;
 
-void _swap(int arr[MAX], int i, int j) {
-	int tmp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = tmp;
+void _swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
-int digitCount(int num) {
+int digitCount(int num)
+{
 	int count = 0;
-	while (num != 0) {
+	while (num != 0)
+	{
 		num /= 10;
 		count++;
 	}
 	return count;
 }
 
-void numToArray(int arr[MAX], int num, int length) {
+void numToArray(int arr[MAX], int num, int length)
+{
 	int i;
-	for (i = length; i > 0; i--) {
+	for (i = length; i > -1; i--)
+	{
 		arr[i] = (num % 10);
 		num /= 10;
 	}
 }
 
-void _sort(int arr[MAX], int i, int j) {
-	while (i > j) {
-		_swap(arr, i, j);
-		i++;
-		j--;
-	}
-}
-
-int arrayToNum(int arr[MAX], int numLength) {
+int arrayToNum(int arr[MAX], int numLength)
+{
 	int num = 0;
 	int i;
-	for (i = numLength; i > 0; i--) {
-		num += (arr[i] * pow(10,(numLength-i)));
+	for (i = numLength; i > -1; i--)
+	{
+		num += (arr[i] * pow(10, (numLength - i)));
 	}
 	return num;
 }
 
-int sol() {
+int sol()
+{
 	int num;
 	cin >> num;
 	int numLength = digitCount(num);
 	int arr[MAX];
 	numToArray(arr, num, numLength);
-	int last = arr[numLength];
-	int i, j;
-	for (i = numLength-1; i >= 0; i--) {
-		if (last > arr[i]) break;
-		last = arr[i];
+	int i;
+	for (i = numLength; i > 0; i--)
+	{
+		if (arr[i] > arr[i-1])
+			break;
 	}
-	if (i >= 0) {
-		for (j = numLength; j >= 0; j--) {
-			if (arr[j] > arr[i]) break;
-		}
-		_swap(arr, i, j);
-		_sort(arr, i+1, numLength);
+	
+	if (i == 1 && arr[i] <= arr[i-1])
+	{
+		return num;
 	}
+
+	int last = arr[i-1], small = i;
+	int j;
+	for (j = i+1; j <= numLength; j++)
+	{
+		if (arr[j] > last && arr[j] < arr[small])
+			small = j;
+	}
+		
+	_swap(&arr[small], &arr[i-1]);
+	sort(arr + i, arr + (numLength+1));
 	num = arrayToNum(arr, numLength);
 	return num;
 }
 
-int main() {
+int main()
+{
 	cout << sol() << endl;
 	return 0;
 }
