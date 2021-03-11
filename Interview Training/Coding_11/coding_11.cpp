894038fb56cacebd7bc14988f2487003e212a817
leftabn <allanbispoxd@gmail.com>
2021-03-11 14:15:07 -0300

    Coding_11 folder

---
+++ Interview Training/Coding_11/coding_11.cpp
@@ -0,0 +1,37 @@
+/**
+ * URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
+ * has sufficient space at the end to hold the additional characters, and that you are given the "true"
+ * length of the string. (Note: If implementing in Java, please use a character array so that you can
+ * perform this operation in place.) 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string sol(string s1, int stringlen)
+{
+  for (int i = s1.length() - 1; i >= 0; i--)
+  {
+    if (isspace(s1[stringlen]))
+    {
+      s1[i--] = '0'; 
+      s1[i--] = '2';
+      s1[i] = '%';
+      stringlen--;
+    }
+    else
+      s1[i] = s1[stringlen--];
+  }
+  return s1;
+}
+
+int main()
+{
+  int realLen;
+  string a;
+  getline(cin, a);
+  cin >> realLen;
+  cout << sol(a, realLen-1) << "\n";
+  return 0;
+}
