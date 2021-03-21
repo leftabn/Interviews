008b143e2e87baf882c395bc07acc12b4adbf4ce
leftabn <allanbispoxd@gmail.com>
2021-03-21 13:21:41 -0300

    coding_15 first bracket of a block stay on the same line

--- Interview Training/coding_15.cpp
+++ Interview Training/coding_15.cpp
@@ -1,51 +1,31 @@
 /**
- * Stack Min: How would you design a stack which, in addition to push and pop, has a function min
- * which returns the minimum element? Push, pop and min should all operate in 0(1) time.
-*/
+ * Stack Min: How would you design a stack which, in addition to push and pop,
+ * has a function min which returns the minimum element? Push, pop and min
+ * should all operate in 0(1) time.
+ */
 
 #include <iostream>
 
 using namespace std;
 
-class Node
-{
+class Node {
 public:
-  Node()
-  {
+  Node() {
     value = 0;
     next = NULL;
     nextMin = NULL;
   }
-  Node(int v)
-  {
+  Node(int v) {
     value = v;
     next = NULL;
     nextMin = NULL;
   }
-  Node *getNext()
-  {
-    return next;
-  }
-  Node *getNextMin()
-  {
-    return nextMin;
-  }
-  int getValue()
-  {
-    return value;
-  }
-  void setValue(int v)
-  {
-    value = v;
-  }
-  void setNext(Node *n)
-  {
-    next = n;
-  }
-  void setNextMin(Node *n)
-  {
-    nextMin = n;
-  }
+  Node *getNext() { return next; }
+  Node *getNextMin() { return nextMin; }
+  int getValue() { return value; }
+  void setValue(int v) { value = v; }
+  void setNext(Node *n) { next = n; }
+  void setNextMin(Node *n) { nextMin = n; }
 
 private:
   int value;
@@ -53,96 +33,67 @@ private:
   Node *nextMin;
 };
 
-class Stack
-{
+class Stack {
 public:
-  Stack()
-  {
+  Stack() {
     top = NULL;
     min = top;
   }
-  int getTop()
-  {
-    return (top->getValue());
-  }
-  int getMin()
-  {
-    return (min->getValue());
-  }
-  void push(Node *n)
-  {
-    if (top == NULL)
-    {
+  int getTop() { return (top->getValue()); }
+  int getMin() { return (min->getValue()); }
+  void push(Node *n) {
+    if (top == NULL) {
       top = n;
       min = top;
-    }
-    else
-    {
+    } else {
       n->setNext(top);
       top = n;
-      if (n->getValue() < min->getValue())
-      {
+      if (n->getValue() < min->getValue()) {
         n->setNextMin(min);
         min = n;
       }
     }
   }
-  void push(int v)
-  {
-    if (top == NULL)
-    {
+  void push(int v) {
+    if (top == NULL) {
       top = new Node(v);
       min = top;
-    }
-    else
-    {
-      Node* tmp = new Node(v);
+    } else {
+      Node *tmp = new Node(v);
       tmp->setNext(top);
       top = tmp;
-      if(v < min->getValue())
-      {
+      if (v < min->getValue()) {
         tmp->setNextMin(min);
         min = tmp;
       }
     }
   }
-  Node *pop()
-  {
+  Node *pop() {
     Node *tmp = top;
     top = top->getNext();
-    if (min == tmp)
-    {
+    if (min == tmp) {
       min = min->getNext();
     }
     return (tmp);
   }
-  void fill(int *arr, int n)
-  {
+  void fill(int *arr, int n) {
     int i;
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
       push(new Node(arr[i]));
     }
   }
-  bool isEmpty()
-  {
-    return (top == NULL);
-  }
-  void clear()
-  {
-    while (top != NULL)
-    {
+  bool isEmpty() { return (top == NULL); }
+  void clear() {
+    while (top != NULL) {
       Node *tmp = top;
       top = pop();
       delete tmp;
     }
   }
-  void printStack()
-  {
+  void printStack() {
     Node *it = top;
     cout << "[";
-    while (it != NULL)
-    {
+    while (it != NULL) {
       cout << it->getValue();
       if (it->getNext())
         cout << ", ";
@@ -156,14 +107,13 @@ private:
   Node *min;
 };
 
-int main()
-{
+int main() {
   Stack s1 = Stack();
   s1.push(10);
   // s1.push(2);
   s1.push(3);
   s1.push(4);
-  //s1.push(1);
+  // s1.push(1);
   cout << s1.getMin() << "\n";
   s1.clear();
   return 0;
