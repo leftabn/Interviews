cbee8f0cafea5f1de995506226fb16fac0edb259
Allan Bispo <leftabn@gmail.com>
2023-03-05 13:14:28 -0300

    Spring security configurado

--- ICarros/api/src/main/java/br/com/icarros/testes/api/configuration/SecurityConfiguration.java
+++ ICarros/api/src/main/java/br/com/icarros/testes/api/configuration/SecurityConfiguration.java
@@ -3,38 +3,41 @@ package br.com.icarros.testes.api.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration
-{
+@EnableWebSecurity
+public class SecurityConfiguration {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers("/api/v1/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/v1/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .logout((logout) -> logout.permitAll());
+
         return http.build();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER_ROLE")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(8);
+        return new InMemoryUserDetailsManager(user);
     }
 }
